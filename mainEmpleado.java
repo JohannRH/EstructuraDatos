594784f794ac4e56974f6232e22ac6363a529191
Sala K 404 <salak404@salasfr.itm.edu.co>
2024-04-26 06:57:01 -0500

    Actualizado

---
+++ mainEmpleado.java
@@ -0,0 +1,28 @@
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class mainEmpleado {
+    public static void main(String[] args) {
+        Scanner cp = new Scanner(System.in);
+        LinkedList<empleado> listaEmpleado = new LinkedList<empleado>();
+
+        for(int i=0; i < 3; i++){
+            empleado e = new empleado();
+
+            System.out.println("EMPLEADO #"+(i+1));
+            System.out.println("Nombre: ");
+            e.setNombre(cp.nextLine());
+            System.out.println("Apellido: ");
+            e.setApellido(cp.nextLine());
+            System.out.println("Direccion: ");
+            e.setDireccion(cp.nextLine());
+            System.out.println("Cargo: ");
+            e.setCargo(cp.nextLine());
+            System.out.println("Edad: ");
+            e.setEdad(cp.nextInt());
+            cp.nextLine();
+            
+            listaEmpleado.add(e);
+        }
+    }
+}
